be1f8a9d56ac14b564c830773a8e19091ad4c2d3
rayman1104 <rayman1104@gmail.com>
2023-06-21 19:56:44 +0200

    remove unused deps

--- app/roles/custom.lua
+++ app/roles/custom.lua
@@ -1,13 +1,3 @@
-local cartridge = require('cartridge')
-
-local function stop()
-    return true
-end
-
-local function validate_config(conf_new, conf_old) -- luacheck: no unused args
-    return true
-end
-
 local function apply_config(conf, opts) -- luacheck: no unused args
     box.schema.func.create('tokio_hyper.start_server', {language = 'C', if_not_exists = true})
     box.schema.user.grant('guest', 'execute', 'function', 'tokio_hyper.start_server', {if_not_exists = true})
@@ -26,9 +16,5 @@ end
 
 return {
     role_name = 'app.roles.custom',
-    init = init,
-    stop = stop,
-    validate_config = validate_config,
     apply_config = apply_config,
-    -- dependencies = {'cartridge.roles.vshard-router'},
 }
--- tarantool-rust-crud-scm-1.rockspec
+++ tarantool-rust-crud-scm-1.rockspec
@@ -7,13 +7,7 @@ source  = {
 dependencies = {
     'tarantool',
     'lua >= 5.1',
-    'checks == 3.1.0-1',
     'cartridge == 2.7.4-1',
-    'metrics == 0.13.0-1',
-    'migrations == 0.4.2-1',
-    'moonwalker',
-    'cartridge-cli-extensions == 1.1.1-1',
-    'expirationd == 1.4.0-1',
 }
 build = {
     type = 'none';
